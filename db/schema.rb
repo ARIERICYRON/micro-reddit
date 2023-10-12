52d3b53f29b8d28648d6b75a298ed8df7cd05636
ariericyron <ariericyron@gmail.com>
2023-10-11 21:45:35 -0400

    feat:create posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_013100) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_12_014354) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
