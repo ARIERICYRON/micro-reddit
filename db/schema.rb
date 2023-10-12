7e79149ea8090ed5a368e50b686f7a3b9e5ac544
ariericyron <ariericyron@gmail.com>
2023-10-11 21:51:08 -0400

    feat:created table for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_014354) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_12_014833) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -28,5 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_014354) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
